0ab43f8893b3e48c53142804af080c9040e712d0
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-02-23 11:36:31 +0000

    adds username from context to ListOfPlants

--- src/screens/ListOfPlants.jsx
+++ src/screens/ListOfPlants.jsx
@@ -3,8 +3,10 @@ import { View, Text, StyleSheet, ScrollView, Image, Button, Pressable } from "re
 import { useNavigation } from "@react-navigation/native";
 import { PlantContext } from "../Contexts/PlantContext";
 import PlantCard from "../Components/PlantCard";
+import { UserContext } from "../Contexts/UserContext";
 
 export default function ListOfPlants() {
+    const { user, setUser } = useContext(UserContext);
     const {myPlantsList} = useContext(PlantContext)
     console.log(myPlantsList)
     const navigation = useNavigation()
@@ -26,6 +28,7 @@ export default function ListOfPlants() {
     } else {
       return (
         <View style={styles.container}>
+          <Text style={styles.blockText}>Welcome {user.username}</Text>
           <Text style={styles.blockText}>You have no plants!</Text>
           <Image source={{uri: "https://i.ibb.co/C9xPQjr/SadPlant.png"}} style={{width: 150, height: 150, marginBottom: 30}}/>
           <Pressable style={styles.button} onPress={() => navigation.navigate("AddPlant")}>
