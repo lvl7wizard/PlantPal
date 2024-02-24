88cdba7adcde06fc2fcf1a7ab2a2d5e11b0e73e2
Ian Graham <i4ng1999@gmail.com>
2024-02-24 23:00:11 +0000

    Converts Dates to using days for both food and water, begun implementing water/feed bars too

--- src/Components/PlantCard.jsx
+++ src/Components/PlantCard.jsx
@@ -1,66 +1,107 @@
-import { View, Text, StyleSheet, Button, Image, Pressable } from "react-native";
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
-export default function PlantCard({plant}) {
-    const { format } = require('date-fns');
-    return (
-        <>
-            <View style={styles.blockText}>
-                <Text style={{color: 'white'}}>Name: {plant.name}</Text>
-            </View>
-            <View style={styles.blockText}>
-                <Text style={{color: 'white'}}>Description: {plant.description}</Text>
-            </View>
-            <View style={styles.blockText}>
-                {/* <Text style={{color: 'white'}}>Water on: {plant.waterDate ? format(plant.waterDate, 'yyyy-MM-dd HH:mm:ss') : 'loading...'}</Text> */}
-                <Text style={{color: 'white'}}>Water in {Math.round((plant.waterDate - Date.now()) / (24 * 3600000))} days</Text>
-            </View>
-            <View style={styles.blockText}>
-                <Text style={{color: 'white'}}>Feed on: {plant.waterDate ? format(plant.foodDate, 'yyyy-MM-dd HH:mm:ss') : 'loading...'}</Text>
-            </View>  
-            <View style={styles.blockText}>
-                <Image source={{ uri: plant.image_url}} style={{width: 150, height: 150}}/>
-            </View>
-            <View>
-               
-            <View style={styles.topRow}>
-                <View>
-                    <Text style={[styles.blockText, styles.text]}>💧 Water Me</Text>
-                    <Text style={[styles.blockText, styles.text]}>🍴 Feed Me</Text>
-                </View>
-                <Pressable style={styles.bottomRightContainer}>
-                    <FontAwesomeIcon icon={ faTrash } color="red" size={30} />
-                </Pressable>
-            </View>
-            </View> 
-        </>
-    )
+import { View, Text, StyleSheet, Button, Image, Pressable } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from 'react';
+
+export default function PlantCard({ plant }) {
+  const { format } = require('date-fns');
+
+  const [waterDays, setWaterDays] = useState(0);
+  const [foodDays, setFoodDays] = useState(0);
+
+  const [waterWidth, setWaterWidth] = useState(0);
+  const [foodWidth, setFoodWidth] = useState(0);
+
+  useEffect(() => {
+    const maxDays = 30;
+    const waterDayz = Math.round(
+      (plant.waterDate - Date.now()) / (24 * 3600000)
+    );
+    const foodDayz = Math.round((plant.foodDate - Date.now()) / (24 * 3600000));
+
+    const waterAmount = (waterDayz / maxDays) * 200;
+    const foodAmount = (foodDayz / maxDays) * 200;
+
+    setWaterDays(waterDayz);
+    setFoodDays(foodDayz);
+    setWaterWidth(waterAmount <= 0 ? 0 : waterAmount);
+    setFoodWidth(foodAmount <= 0 ? 0 : foodAmount);
+  }, []);
+
+  return (
+    <>
+      <View style={styles.blockText}>
+        <Text style={{ color: 'white' }}>Name: {plant.name}</Text>
+      </View>
+      <View style={styles.blockText}>
+        <Text style={{ color: 'white' }}>Description: {plant.description}</Text>
+      </View>
+      <View style={styles.blockText}>
+        <Text style={{ color: 'white' }}>
+          Water:{' '}
+          {plant.waterDate
+            ? waterDays === 0
+              ? 'Today'
+              : `${waterDays} days`
+            : 'loading...'}
+        </Text>
+      </View>
+
+      <View style={styles.blockText}>
+      <Text style={{ color: 'white' }}>
+          Feed:{' '}
+          {plant.foodDate
+            ? foodDays === 0
+              ? 'Today'
+              : `${foodDays} days`
+            : 'loading...'}
+        </Text>
+      </View>
+      <View style={styles.blockText}>
+        <Image
+          source={{ uri: plant.image_url }}
+          style={{ width: 150, height: 150 }}
+        />
+      </View>
+      <View>
+        <View style={styles.topRow}>
+          <View>
+            <Text style={[styles.blockText, styles.text]}>💧 Water Me</Text>
+            <Text style={[styles.blockText, styles.text]}>🍴 Feed Me</Text>
+          </View>
+          <Pressable style={styles.bottomRightContainer}>
+            <FontAwesomeIcon icon={faTrash} color="red" size={30} />
+          </Pressable>
+        </View>
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    blockText: {
-        marginBottom: 10,
-    },
-    button: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginVertical: 10,
-        color: "white",
-        width: "min-content",
-        padding: 10
-      },
-    text: {
-        color: "white"
-    },
-    topRow: {
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-      },
-    bottomRightContainer: {
-        position: 'absolute',
-        bottom: 0,
-        right: 0,
-        flexDirection: 'row',
-        alignItems: 'flex-end',
-    },
-})
+  blockText: {
+    marginBottom: 10,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    color: 'white',
+    width: 'min-content',
+    padding: 10,
+  },
+  text: {
+    color: 'white',
+  },
+  topRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  bottomRightContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
+});
