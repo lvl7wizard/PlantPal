637c9bd7c81e2fde79f3bf09a5ae02d12bb04eec
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-02-28 16:35:59 +0000

    adds styling to plantcards

---
+++ src/StyledComponents/IconButton.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+
+const IconButton = styled.Pressable`
+border-radius: 10px; 
+border-width: 1px; 
+border-color: black;
+padding: 10px; 
+background-color: rgba(0, 0, 0, 0.2);
+`
+export default IconButton
